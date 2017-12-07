729e45a5963a57346305c86b2905cfc356793f9d
michalch23 <michalchyb1@gmail.com>
2017-12-07 16:35:10 +0100

    abs(x) added

--- src/Practice.java
+++ src/Practice.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 
 public class Practice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		bezwgledna();
+
+	}
+
+	private static void bezwgledna() {
+		int x, wynik;
+
+		System.out.println("Podaj wartoœæ x: ");
+		Scanner scan = new Scanner(System.in);
+		x = scan.nextInt();
+		System.out.println(wynik = Math.abs(x));
 
 	}
 
