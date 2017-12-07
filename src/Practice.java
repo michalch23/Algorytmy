90e0a87fa312b82e963c3719dfda0c4fd94f46bf
michalch23 <michalchyb1@gmail.com>
2017-12-07 17:17:43 +0100

    sumaElementowTablicy added

--- src/Practice.java
+++ src/Practice.java
@@ -3,7 +3,27 @@ import java.util.Scanner;
 public class Practice {
 
 	public static void main(String[] args) {
-		bezwgledna();
+		// bezwgledna();
+		sumaElementowTablicy();
+
+	}
+
+	private static void sumaElementowTablicy() {
+		int n, wynik = 0;
+		System.out.println("Podaj liczbe elementow tablicy");
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		int[] liczby = new int[n];
+		// uzupelnianie tablicy
+		for (int i = 0; i < n; i++) {
+			System.out.println("Podaj element a [" + i + "]");
+			liczby[i] = scan.nextInt();
+		}
+		// sumowanie elementow tablicy
+		for (int i = 0; i < n; i++) {
+			wynik += liczby[i];
+		}
+		System.out.println("Wynik sumowania wynosi: " + wynik);
 
 	}
 
